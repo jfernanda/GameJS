edc38a55e3eab1a4d7b4ffb09bb9cb335a247c62
J. Fernanda <santosl.jfer@gmail.com>
2024-09-30 21:26:23 +0100

    Construção da lógica do jogo

---
+++ app.js
@@ -0,0 +1,10 @@
+alert("Este é o jogo do número secreto");
+let secretNumber = 5;
+let chute = prompt("Digite um número entre 1 e 10");
+
+if (chute == secretNumber) {
+    console.log("Acertou miserávi");
+}
+
+let mensagemDeBoasVindas = 'Boas vindas ao App';
+alert('mensagemDeBoasVindas');
