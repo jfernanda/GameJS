13b877bc02ec9aad35812b7269814fc6e3b913a8
J. Fernanda <santosl.jfer@gmail.com>
2024-10-08 22:57:15 +0100

    operadores ternários, while, math.random

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 alert("Este é o jogo do número secreto");
-let secretNumber = 9;
-let bet = prompt("Digite um número entre 1 e 10");
+let maxNumber = 5000;
+let secretNumber = parseInt(Math.random() * maxNumber + 1);
+let bet ;
+let tryNumbers = 1;
 
-if (bet == secretNumber) {
-    alert(`Acertou miserávi, era o número ${secretNumber}`);
-} else {
-    alert("Poxa... Tente de novo!")
+while (bet != secretNumber) {
+    bet = prompt(`Digite um número entre 1 e ${maxNumber}`);
+    if (bet == secretNumber) {
+        break;
+    } else {
+        if (bet > secretNumber) {
+            alert(`O número é menor que ${bet}`);
+        } else {
+            alert (`O número é maior que ${bet}`);
+        }
+        tryNumbers++;
+    }
 }
+
+let tryWord = tryNumbers > 1 ? 'tentativas' : 'tentativa';
+alert(`Acertou miserávi, era o número ${secretNumber}, só demorou ${tryNumbers} ${tryWord} `);
